481f9f4cb00c7ca98e83c6822e24d06ad0fa3607
kvrigor <kvrigor@users.noreply.github.com>
2025-10-06 18:50:43 +0200

    Switched back to https

--- oily/container/install-dirty-oils.sh
+++ oily/container/install-dirty-oils.sh
@@ -8,7 +8,7 @@ doas apk add readline-dev
 mkdir build
 cd build
 
-wget http://op.oilshell.org/uuu/github-jobs/10248/cpp-tarball.wwz/_release/oils-for-unix.tar
+wget https://op.oilshell.org/uuu/github-jobs/10248/cpp-tarball.wwz/_release/oils-for-unix.tar
 
 tar xvf oils-for-unix.tar
 rm oils-for-unix.tar
